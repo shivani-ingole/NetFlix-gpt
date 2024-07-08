d3a7dd25fbb631bc9e523532c2c64878925914cc
shivani-ingole <shivaniingole1996@gmail.com>
2024-07-08 14:42:25 +0530

    updated app js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1 className='text-3xl font-bold text-red-900'>Hello Netflix GPT</h1>
+      <h1 className='text-3xl font-bold text-red-900'>Hello Netflix GPT Application</h1>
 
     </div>
   );
